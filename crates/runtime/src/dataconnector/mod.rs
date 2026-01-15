410220a07997c207cf213afc21d6ab02514220bd
Phillip LeBlanc <phillip@leblanc.tech>
2026-01-15 20:21:05 +0900

    feat: extract sharepoint connector to separate crate

--- crates/runtime/src/dataconnector/mod.rs
+++ crates/runtime/src/dataconnector/mod.rs
@@ -160,8 +160,6 @@ pub mod parameters;
 pub mod s3;
 #[cfg(feature = "ftp")]
 pub mod sftp;
-#[cfg(feature = "sharepoint")]
-pub mod sharepoint;
 pub mod sink;
 #[cfg(feature = "smb")]
 pub mod smb;
